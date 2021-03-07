2a1428fd5bd4c34439e54c48c91f16401526062a
Sebastian Schubert <citox91@gmail.com>
2021-03-07 22:46:57 +0100

    FirstCommit 0.1

---
+++ Assets/NewTestScript.cs
@@ -0,0 +1,26 @@
+﻿/*
+*Copyright (c) Sebastian Schubert
+*/
+
+using UnityEngine;
+
+public class NewTestScript : MonoBehaviour {
+
+    #region Variables
+    #endregion
+
+    #region Unity Methods
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+}
